67fcb3a3e3f60edf668b2ea09155cd2cd47f0b74
SAMEERAABOQAYAS <sameera.aboqayas@gmail.com>
2020-04-22 16:56:59 +0200

    week1.py

---
+++ week4-2.py
@@ -0,0 +1,14 @@
+week4-2.py
+...
+print(enter student's name) :
+S1=['Ahmad',18,17,19,5,8,25]
+S2=['Sami',20,20,19,9,28]
+S3=['faris',14,5,16,13,7]
+...
+s=input("enter student's name:")
+if s in s1:
+    print(s, sum(s1[1:]))
+elif s in s2:
+    print(s, sum(s2[2:]))
+elif s in s3:
+    print(s, sum(s3]) )
